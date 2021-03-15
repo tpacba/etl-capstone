3ffdef08cca8dcfd3f9973b425057a5fc5c6bb96
tpacba <tpacba@live.com>
2021-03-15 17:28:13 +0000

    cleaned up pandas data, dropped duplicates

--- data-visuals/mongo_request.py
+++ data-visuals/mongo_request.py
@@ -1,6 +1,5 @@
 from pymongo import MongoClient
 from bson import json_util
-import datetime
 
 import pandas
 import json
@@ -13,8 +12,21 @@ list_data = []
 
 for item in cursor:
     item = json.loads(json_util.dumps(item))
-    list_data.append(item)
-    print(item)
+    list_data.append(
+        {
+            'airTemperature': item['airTemperature'],
+            'gust': item['gust'],
+            'humidity': item['humidity'],
+            'lat': item['lat'],
+            'lng': item['lng'],
+            'station_id': item['station_id'],
+            'time': item['time'],
+            'uvIndex': item['uvIndex'],
+            'windSpeed': item['windSpeed']
+        }
+    )
 
 pd_obj = pandas.read_json(json.dumps(list_data), orient='records')
-pd_obj.to_csv('./data-visuals/station_data.csv')
+pd_df = pd_obj.drop_duplicates()
+print(pd_df)
+pd_df.to_csv('./data-visuals/station_data.csv')
